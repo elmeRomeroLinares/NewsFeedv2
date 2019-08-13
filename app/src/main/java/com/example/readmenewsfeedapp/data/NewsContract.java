6ff29403082485b8cc9ed442a7a8ccc2c271f7b5
elmeRomeroLinares <52327494+elmeRomeroLinares@users.noreply.github.com>
2019-08-13 09:49:28 -0600

    Update NewsContract.java

--- app/src/main/java/com/example/readmenewsfeedapp/data/NewsContract.java
+++ app/src/main/java/com/example/readmenewsfeedapp/data/NewsContract.java
@@ -1,4 +1,24 @@
 package com.example.readmenewsfeedapp.data;
 
+import android.provider.BaseColumns;
+
 public class NewsContract {
+
+    // private constructor to avoid instantiation
+    private NewsContract(){}
+
+    public static final class NewsEntry implements BaseColumns {
+
+        // table savedNews name constant
+        private static final String TABLE_NAME = "savedNews";
+
+        // table savedNews columns
+        public static final String _ID = BaseColumns._ID;
+        public static final String COLUMN_ARTICLE_HEADLINE = "headline";
+        public static final String COLUMN_ARTICLE_SECTION = "section";
+        public static final String COLUMN_ARTICLE_THUMBNAIL = "thumbnail";
+        public static final String COLUMN_ARTICLE_BODY = "body";
+    }
+
+
 }
